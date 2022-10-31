cb7b6d232bc4a407c6147ac06be18fac8e2c108b
rahul.chander <rahul.chander@epitech.eu>
2022-10-31 18:27:46 +0100

    [ADD] flag %G

--- source/flag_g.c
+++ source/flag_g.c
@@ -19,3 +19,16 @@ int my_put_g(double nb)
     }
     return nbr_len;
 }
+
+int my_put_big_g(double nb)
+{
+    long int e = my_get_exponent(nb);
+    int nbr_len = 0;
+
+    if (e < -4 || e > 6) {
+        nbr_len = scientific_notation_big_e(nb, 6);
+    } else {
+        nbr_len = my_put_float(nb, 8);
+    }
+    return nbr_len;
+}
