1f4412d3a92f2e27010b0f5e86ad6840c6742691
rahul.chander <rahul.chander@epitech.eu>
2022-10-31 14:24:10 +0100

    [ADD] Flag %f, %e, %E

--- source/big_scientific.c
+++ source/big_scientific.c
@@ -7,45 +7,84 @@
 
 #include "../include/my.h"
 
-int my_get_exponent(double nb)
+int my_get_exponent_s(double nb)
 {
-    int n = 1;
+    unsigned long int n = 1;
     int i = 1;
 
-    if (nb < 10) {
+    if ((long int) nb == 0) {
+        while ((long int) nb == 0) {
+            nb = nb * my_compute_power_rec(10, 1);
+            i++;
+        }
+        n = (long int) nb;
+        return my_nb_len(n) - i;
+    }
+    if (nb < 10)
         return 1;
-    }
-    while (nb > n) {
+    while (nb >= 10) {
         i++;
-        n = n * 10;
+        nb = nb / 10;
     }
-    return i - 1;
+    return i;
 }
 
-void my_displays_big_e(long int e, double nb)
+void my_display_big_e_second(long int e, double nb, long int e_sign)
+{
+    if (e < 10) {
+        if (e_sign == 1) {
+            my_putstr("E-0");
+            my_put_nbr(e);
+        } else {
+            my_putstr("E+0");
+            my_put_nbr(e - 1);
+        }
+        return;
+    } else {
+        if (e_sign == 1) {
+            my_putstr("E-");
+            my_put_nbr(e);
+        } else {
+            my_putstr("E+");
+            my_put_nbr(e - 1);
+        }
+        return;
+    }
+}
+
+void my_displays_big_e(long int e, double nb, long int e_sign)
 {
     my_put_float(nb);
     if (e == 1) {
-        my_putstr("E+00");
+        if (e_sign == 0)
+            my_putstr("E+00");
+        if (e_sign == 1)
+            my_putstr("E-01");
         return;
     }
-    if (e < 10) {
-        my_putstr("E+0");
-        my_put_nbr(e - 1);
-        return;
-    }
+    my_display_big_e_second(e, nb, e_sign);
 }
 
 int scientific_notation_big_e(double nb)
 {
-    long int s = 0;
     long int e = 0;
+    long int e_sign = 0;
     union bit_float data;
 
     data.f = nb;
-    s = data.a >> 31;
-    e = my_nb_len((unsigned long int) nb);
-    nb = nb / my_compute_power_rec(10, e - 1);
-    my_displays_e(e, nb);
-    return 0;
+    if ((data.a >> 31) == 1) {
+        nb = nb * (-1);
+        my_putchar('-');
+    }
+    e = my_get_exponent_s(nb);
+    if (e < 0) {
+        e = e * (-1);
+        e_sign++;
+        while ((long int) nb == 0)
+            nb = nb * my_compute_power_rec(10, 1);
+    } else
+        while (nb > 10)
+            nb = nb / my_compute_power_rec(10, 1);
+    my_displays_big_e(e, nb, e_sign);
+    return (data.a >> 31) + 8 + 4;
 }
--- source/my_put_float.c
+++ source/my_put_float.c
@@ -7,16 +7,11 @@
 
 #include "../include/my.h"
 
-int display_float(long int s, int pow,
-                long int w, long int d)
+int display_float(int pow, long int w,
+long int d)
 {
-    int a = 0;
     int i = 0;
     int n = my_nb_len(d);
-    if (s == 1) {
-        my_putchar('-');
-        a++;
-    }
     my_put_nbr(w);
     my_putchar('.');
     while (i < pow - n) {
@@ -24,28 +19,28 @@ int display_float(long int s, int pow,
         i++;
     }
     my_put_nbr(d);
-    return n + s + my_nb_len(w);
+    return n + my_nb_len(w);
 }
 
 int my_put_float(double nb)
 {
-    long int s = 0;
-    long int e = 0;
-    long int m = 0;
-    long int w_part = 0;
-    long int w_part_display = 0;
-    unsigned long int d_part = 0;
+    long long int w_part_display = 0;
+    double d_part = 0;
     int nb_len_return = 0;
     int pow = 6;
     union bit_float data;
 
     data.f = nb;
-    s = data.a >> 31;
-    e = (data.a >> 23) & 0b0111111110000000000000000000000;
-    m = data.a  & 0b00000000011111111111111111111111;
-    w_part_display = (unsigned long int) nb;
-    d_part = (unsigned long int) ((nb - w_part_display) *
-    my_compute_power_rec(10, pow));
-    nb_len_return = display_float(s, pow, w_part_display, d_part);
-    return nb_len_return;
+    if ((data.a >> 31) == 1) {
+        nb = nb * (-1);
+        my_putchar('-');
+    }
+    w_part_display = (long long int) nb;
+    d_part = nb - w_part_display;
+    while (pow > 0) {
+        d_part = d_part * 10;
+        pow--;
+    }
+    nb_len_return = display_float(pow, w_part_display, d_part);
+    return nb_len_return + (data.a >> 31);
 }
